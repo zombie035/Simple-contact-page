01348698bd19511a5c4677edff24a06d485f8121
zombie035 <167063563+zombie035@users.noreply.github.com>
2025-12-17 16:07:57 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hostRouter = require('./Hostrouter');
+const rootDir = require('./pathUtils');
+
+const app = express();
+const path = require("path")
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', hostRouter);
+
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(rootDir, '404.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
